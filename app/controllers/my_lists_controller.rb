4b7e0187efad1a65d4cf21a2ac2f0051914c3d88
hitoshi <murozono1030@gmail.com>
2018-03-01 13:47:07 +0900

    portfolio commit

--- app/controllers/my_lists_controller.rb
+++ app/controllers/my_lists_controller.rb
@@ -3,13 +3,13 @@ class MyListsController < ApplicationController
 	def create
 		my_list = current_user.my_lists.new(video_id: params[:format])
 		my_list.save
-		# redirect_to root_path
+		redirect_to root_path
 	end
 
 	def destroy
 		my_list = current_user.my_lists.find_by(video_id: params[:id])
 		my_list.destroy
-		# redirect_to root_path
+		redirect_to root_path
 	end
 
 	def index
